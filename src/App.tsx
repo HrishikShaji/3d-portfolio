acae1ecd25c7ac49a89ecd1d0c3388035dd0c35b
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-04-15 18:17:38 +0530

    initial

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text-3xl text-red-500" >
+        Portfolio
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
