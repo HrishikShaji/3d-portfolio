295fb66c6896a6f926e1b6b983c1a6ebb04d45bb
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-04-16 11:42:35 +0530

    experience section done

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Showcase from './components/sections/Showcase'
 import Navbar from './components/Navbar'
 import LogoSection from './components/LogoSection'
 import FeatureCards from './components/FeatureCards'
+import ExperienceSection from './components/sections/ExperienceSection'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,8 @@ function App() {
       <Showcase />
       <LogoSection />
       <FeatureCards />
+      <ExperienceSection />
+
     </>
   )
 }
---
+++ src/components/sections/TitleHeader.tsx
@@ -0,0 +1,15 @@
+interface Props {
+	title: string;
+	sub: string;
+}
+
+export default function TitleHeader({ title, sub }: Props) {
+	return <div className="flex flex-col items-center gap-5">
+		<div className="hero-badge">
+			<p>{sub}</p>
+		</div>
+		<div className="font-semibold md:text-5xl text-3xl text-center">
+			{title}
+		</div>
+	</div>
+}
